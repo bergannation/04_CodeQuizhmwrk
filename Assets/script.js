a45f2605081cf7dc89430ff98e16721c99319bd8
Travis Brown <bergannation@gmail.com>
2021-04-05 11:43:10 -0500

    started boilerplate for quiz and css

---
+++ Assets/script.js
@@ -0,0 +1,7 @@
+// Timer in the top right
+// view highscores in the top left
+
+// In the middle, we need to have a start quiz button and a description of that starting quiz
+//The timer will start when the user clicks start quiz button.
+// If the user clicks a wrong answer, the timer deducts 7 seconds as a penalty
+// each question should have four answers as a multiple choice
