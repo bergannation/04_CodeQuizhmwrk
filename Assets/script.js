c67d6dbae22678689620761be60cc51df4812c31
Travis Brown <bergannation@gmail.com>
2021-04-05 18:48:02 -0500

    updating new JS additions

--- Assets/script.js
+++ Assets/script.js
@@ -5,3 +5,29 @@
 //The timer will start when the user clicks start quiz button.
 // If the user clicks a wrong answer, the timer deducts 7 seconds as a penalty
 // each question should have four answers as a multiple choice
+
+// Selects element by class
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 75;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if (secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+
+setTime();
+
+var questions = [];
